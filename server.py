ca368a94fdd0292c26af56e433d3bb1e7dc9bcda
Eliphaz BOUYE <eliphazbouye@gmail.com>
2023-02-08 17:35:20 +0000

    database connection

--- server.py
+++ server.py
@@ -1,14 +1,22 @@
-from flask import Flask,redirect
-
+from flask import Flask,redirect, request, render_template
+from .db import db
+from .Model import Url
 app = Flask(__name__)
+
 @app.route('/')
 def index():
-    return "Flask URL-shortener"
+    return render_template("index.html")
 
-@app.post('/create')
+@app.route('/create', methods=['POST'])
 def create():
+    if request.method == 'POST':
+        url = request.form['url']
+        short = Url(url=url, alias='ttttt')
+        db.create_all()
+        db.session.add(short)
 
+    return render_template("result.html",result = Short.query.all())
 
-@app.route('/<alias>')
-def short(alias):
-    return redirect("https://github.com/eliphazbouye")
+# @app.route('/<alias>')
+# def short(alias):
+#     return redirect("https://github.com/eliphazbouye")
