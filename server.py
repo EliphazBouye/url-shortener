3f13a159c0c06ad9991ac8b8890314a9f524a1c8
Eliphaz BOUYE <eliphazbouye@gmail.com>
2023-02-09 15:17:09 +0000

    add url, get alias, redirection

--- server.py
+++ server.py
@@ -1,7 +1,25 @@
 from flask import Flask,redirect, request, render_template
-from .db import db
-from .Model import Url
+from dotenv import load_dotenv, dotenv_values
+from flask_sqlalchemy import SQLAlchemy
+import string
+import random
+
 app = Flask(__name__)
+
+load_dotenv()
+config = dotenv_values(".env")
+
+db = SQLAlchemy()
+
+app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql://{config['DB_USER']}:{config['DB_PASS']}@localhost/{config['DB_NAME']}"
+db.init_app(app)
+
+# MODEL
+class Url(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(250), unique=True, nullable=False)
+    alias = db.Column(db.String(250))
+
 
 @app.route('/')
 def index():
@@ -11,12 +29,29 @@ def index():
 def create():
     if request.method == 'POST':
         url = request.form['url']
-        short = Url(url=url, alias='ttttt')
+        KEY_LEN = 5
+        def base_str():
+            return (string.ascii_letters+string.digits)
+
+        def key_gen():
+            keylist = [random.choice(base_str()) for i in range(KEY_LEN)]
+            return "".join(keylist)
+
+        short = Url(url=url, alias=key_gen())
         db.create_all()
         db.session.add(short)
+        db.session.commit()
 
-    return render_template("result.html",result = Short.query.all())
+    return redirect('all_short')
 
-# @app.route('/<alias>')
-# def short(alias):
-#     return redirect("https://github.com/eliphazbouye")
+@app.route('/all_short')
+def all_short():
+    return render_template("result.html", result = Url.query.all(), base = request.url_root)
+
+@app.route('/<alias>')
+def alias(alias):
+    res = db.session.query(Url).filter(Url.alias == alias)
+    url = []
+    for row in res:
+        url.append(row.url)
+    return redirect(url[0])
